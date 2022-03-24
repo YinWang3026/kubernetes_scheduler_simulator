b9ab053e2fb2e9a83cf9fb13b4cf07178b6920d3
Yin Wang <yw2178@nyu.edu>
2022-03-24 11:18:03 -0400

    fixed pod and node class

--- simulator.py
+++ simulator.py
@@ -53,6 +53,18 @@ class Pod:
         return "ExecStartTime: %d, FinishTime: %d, TotalWaitTime: %d" \
             % (self.execStartTime, self.finishTime, self.totalWaitTime)
 
+class PodList:
+    def __init__(self) -> None:
+        self.pods = []
+
+    def addPod(self, pod: Pod) -> None:
+        self.pods.append(pod)
+
+    def __repr__(self) -> str:
+        s = "Pod List:\n"
+        for i in self.pods:
+            s += "\t" + i.__repr__() + "\n"
+
 class Node:
     def __init__(self, name: str, cpu: int, gpu: int, ram: int) -> None:
         # Basic info - DOES NOT CHANGE
@@ -61,13 +73,33 @@ class Node:
         self.gpu = gpu
         self.ram = ram
 
+        # Remaining resource
+        self.curCpu = cpu
+        self.curGpu = gpu
+        self.curRam = ram
+
         # Pods running on this node
-        self.podSet = set() # For O(1) remove and add
+        self.podSet = set() # For O(1) remove and add, but can use other data struct if needed
     
     def addPod(self, pod: Pod) -> None:
+        self.curCpu -= pod.cpu
+        if self.curCpu < 0:
+            print("Node %s CPU went negative" % (self.name))
+            exit(1)
+        self.curGpu -= pod.gpu
+        if self.curGpu < 0:
+            print("Node %s GPU went negative" % (self.name))
+            exit(1)
+        self.curRam -= pod.ram
+        if self.curRam < 0:
+            print("Node %s RAM went negative" % (self.name))
+            exit(1)
         self.podSet.add(pod)
 
     def removePod(self, pod: Pod) -> None:
+        self.curCpu += pod.cpu
+        self.curGpu += pod.gpu
+        self.curRam += pod.ram
         self.podSet.discard(pod)
     
     def __repr__(self) -> str:
@@ -75,10 +107,42 @@ class Node:
             % (self.name, self.cpu, self.gpu, self.ram)
     
     def reprPodList(self) -> str:
-        s = "Node[%s] Pod List:\n" % (self.name)
+        s = "Node[%s] Pod List: " % (self.name)
         for pod in self.podSet:
-            s += "\t" + pod.name + "\n"
+            s += pod.name + " "
         return s
+
+class NodeList:
+    def __init__(self) -> None:
+        self.nodes = []
+
+    def addNode(self, node: Node) -> None:
+        self.nodes.append(node)
+
+    def getMatch(self, pod: Pod) -> list[Node]:
+        # Return a list of nodes that can run the given pod
+        # Can use derived class and a custom policy
+        # Default policy, return the first node that has enough resource to run this pod
+        for i in self.nodes:
+            if i.curCpu >= pod.cpu and i.curGpu >= pod.gpu and i.curRam >= pod.ram:
+                return [i]
+        
+        return []
+
+    def __repr__(self) -> str:
+        s = "Node List:\n"
+        for i in self.nodes:
+            s += "\t" + i.__repr__() + "\n"
+
+class NodeListSecretPolicy(NodeList):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def getMatch(self, pod: Pod) -> list[Node]:
+        # return super().getMatch(pod)
+        # SuPer secret policy
+        pass
+
 
 eventID = 0 # Global event ID tracker
 class Event:
