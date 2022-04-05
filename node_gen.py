33b7f53ebcd01a75beb5e2e5dbfdf758b5bc1757
Yin Wang <yw2178@nyu.edu>
2022-04-05 19:27:16 -0400

    fixed node gen

--- node_gen.py
+++ node_gen.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, choice
 from name_generator import iter_all_strings
 
 gen = iter_all_strings()
@@ -12,8 +12,8 @@ with open(outFile, 'w') as f:
             f.write("\n")
 
         nodeName = "Node" + next(gen)
-        cpu = randint(10,16)
-        gpu = randint(10,32)
-        ram = randint(32,128)
+        cpu = choice([4,8,16,32,64,128])
+        gpu = choice([1,2,4,8])
+        ram = choice([4,8,16,32,64,128])
 
         f.write("%s %d %d %d" % (nodeName, cpu, gpu, ram))
