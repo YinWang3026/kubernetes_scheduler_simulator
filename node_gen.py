2476bd620dee1945da405ccfe102ccea1651df90
Yin Wang <yw2178@nyu.edu>
2022-04-08 11:41:35 -0400

    allow file name for gen

--- node_gen.py
+++ node_gen.py
@@ -1,19 +1,24 @@
 from random import randint
 from name_generator import iter_all_strings
+import sys
 
-gen = iter_all_strings()
-N = 15
-outFile = "nodes.txt"
+def main(argv: list[str]):
+    gen = iter_all_strings()
+    N = 15
+    outFile = argv[0] if len(argv) == 1 else "nodes.txt"
 
-with open(outFile, 'w') as f:
-    f.write("nodeName cpu gpu ram\n")
-    for i in range(0, N):
-        if i != 0:
-            f.write("\n")
+    with open(outFile, 'w') as f:
+        f.write("nodeName cpu gpu ram\n")
+        for i in range(0, N):
+            if i != 0:
+                f.write("\n")
 
-        nodeName = "Node" + next(gen)
-        cpu = randint(10,16)
-        gpu = randint(10,32)
-        ram = randint(32,128)
+            nodeName = "Node" + next(gen)
+            cpu = randint(10,16)
+            gpu = randint(10,32)
+            ram = randint(32,128)
 
-        f.write("%s %d %d %d" % (nodeName, cpu, gpu, ram))
+            f.write("%s %d %d %d" % (nodeName, cpu, gpu, ram))
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
--- pod_gen.py
+++ pod_gen.py
@@ -1,25 +1,31 @@
 from random import randint
 from name_generator import iter_all_strings
+import sys
 
-currentTime = 0
-gen = iter_all_strings()
-N = 5
-outFile = "pods.txt"
+def main(argv):
+    currentTime = 0
+    gen = iter_all_strings()
+    N = 5
+    outFile = argv[0] if len(argv) == 1 else "pods.txt"
 
-with open(outFile, 'w') as f:
-    f.write("podName arrivalTime work prio tickets cpu gpu ram\n")
-    for i in range(0, N):
-        if i != 0:
-            f.write("\n")
+    with open(outFile, 'w') as f:
+        f.write("podName arrivalTime work prio tickets cpu gpu ram\n")
+        for i in range(0, N):
+            if i != 0:
+                f.write("\n")
 
-        podName = "Pod" + next(gen)
-        arrivalTime = randint(0, 5)
-        work = randint(100, 1000)
-        prio = randint(1, 4)
-        tickets = randint(10, 100)
-        cpu = randint(1,16)
-        gpu = randint(1,32)
-        ram = randint(1,128)
+            podName = "Pod" + next(gen)
+            arrivalTime = randint(0, 5)
+            work = randint(100, 1000)
+            prio = randint(1, 4)
+            tickets = randint(10, 100)
+            cpu = randint(1,16)
+            gpu = randint(1,32)
+            ram = randint(1,128)
 
-        currentTime += arrivalTime
-        f.write("%s %d %d %d %d %d %d %d" % (podName, currentTime, work, prio, tickets, cpu, gpu, ram))
+            currentTime += arrivalTime
+            f.write("%s %d %d %d %d %d %d %d" % (podName, currentTime, work, prio, tickets, cpu, gpu, ram))
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
