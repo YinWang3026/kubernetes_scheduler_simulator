3937f5bfbb240abd6404287eba594f709dc15a55
Yin Wang <yw2178@nyu.edu>
2022-04-27 09:38:51 -0400

    gen nodes and pods

--- node_gen.py
+++ node_gen.py
@@ -4,21 +4,39 @@ import sys
 
 def main(argv: list[str]):
     gen = iter_all_strings()
-    N = 15
+    N = 60
     outFile = argv[0] if len(argv) == 1 else "nodes.txt"
+
+    cpus = [4,8,16]
+    rams = [8,16,32]
+    gpus = [1,2,4]
 
     with open(outFile, 'w') as f:
         f.write("nodeName cpu gpu ram\n")
-        for i in range(0, N):
+        for i in range(0, int(N*0.4)):
             if i != 0:
                 f.write("\n")
 
             nodeName = "Node" + next(gen)
-            cpu = randint(10,16)
-            gpu = randint(10,32)
-            ram = randint(32,128)
+            # cpu = randint(10,16)
+            # gpu = randint(10,32)
+            # ram = randint(32,128)
 
-            f.write("%s %d %d %d" % (nodeName, cpu, gpu, ram))
+            f.write("%s %d %d %d" % (nodeName, cpus[0], gpus[0], rams[0]))
+        f.write("\n")
+        for i in range(0, int(N*0.4)):
+            if i != 0:
+                f.write("\n")
+
+            nodeName = "Node" + next(gen)
+            f.write("%s %d %d %d" % (nodeName, cpus[1], gpus[1], rams[1]))
+        f.write("\n")
+        for i in range(0, int(N*0.2)):
+            if i != 0:
+                f.write("\n")
+
+            nodeName = "Node" + next(gen)
+            f.write("%s %d %d %d" % (nodeName, cpus[2], gpus[2], rams[2]))
 
 if __name__ == "__main__":
    main(sys.argv[1:])
--- pod_gen.py
+++ pod_gen.py
@@ -4,19 +4,26 @@ import sys
 import random
 
 def main(argv):
-    currentTime = 0
-    gen = iter_all_strings()
-    N = 5
-    num_users = 5
     outFile = argv[0] if len(argv) == 1 else "pods.txt"
+    N = 1800 # number of pods
+    num_users = 30 # number of users
 
     userNames = []
     userPriorities = dict()
+    genUserNames = iter_all_strings()
     for _ in range(num_users):
-        name = "User" + next(gen)
+        name = "User" + next(genUserNames)
         userNames.append(name)
         userPriorities[name] = randint(1,10)
 
+    cpus = [4,8,16]
+    rams = [8,16,32]
+    gpus = [1,2,4]
+
+    # Large pods
+    outFile = "pods_large.txt"
+    genPodNames = iter_all_strings()
+    currentTime = 0
     with open(outFile, 'w') as f:
         f.write("UserName podName arrivalTime work prio tickets cpu gpu ram tickets\n")
         for i in range(0, N):
@@ -24,18 +31,63 @@ def main(argv):
                 f.write("\n")
 
             userName = random.choice(userNames)
-            podName = "Pod" + next(gen)
+            podName = "Pod" + next(genPodNames)
             arrivalTime = randint(0, 5)
             work = randint(100, 1000)
             prio = randint(1, 4)
             tickets = userPriorities[userName] * 10
-            cpu = randint(1,16)
-            gpu = randint(1,32)
-            ram = randint(1,128)
+            cpu = randint(cpus[1],cpus[2]) # Only has cpus from 8 to 16
+            gpu = randint(gpus[1],gpus[2])
+            ram = randint(rams[1],rams[2])
+
+            currentTime += arrivalTime
+            f.write("%s %s %d %d %d %d %d %d %d" % (userName, podName, currentTime, work, prio, tickets, cpu, gpu, ram))
+    
+    # Small pods
+    outFile = "pods_small.txt"
+    genPodNames = iter_all_strings()
+    currentTime = 0
+    with open(outFile, 'w') as f:
+        f.write("UserName podName arrivalTime work prio tickets cpu gpu ram tickets\n")
+        for i in range(0, N):
+            if i != 0:
+                f.write("\n")
+
+            userName = random.choice(userNames)
+            podName = "Pod" + next(genPodNames)
+            arrivalTime = randint(0, 5)
+            work = randint(100, 1000)
+            prio = randint(1, 4)
+            tickets = userPriorities[userName] * 10
+            cpu = randint(1,cpus[1]) # Only has cpus from 1 to 8
+            gpu = randint(0,gpus[1])
+            ram = randint(2,rams[1])
+
+            currentTime += arrivalTime
+            f.write("%s %s %d %d %d %d %d %d %d" % (userName, podName, currentTime, work, prio, tickets, cpu, gpu, ram))
+    
+    # mix pods
+    outFile = "pods_mix.txt"
+    genPodNames = iter_all_strings()
+    currentTime = 0
+    with open(outFile, 'w') as f:
+        f.write("UserName podName arrivalTime work prio tickets cpu gpu ram tickets\n")
+        for i in range(0, N):
+            if i != 0:
+                f.write("\n")
+
+            userName = random.choice(userNames)
+            podName = "Pod" + next(genPodNames)
+            arrivalTime = randint(0, 5)
+            work = randint(100, 1000)
+            prio = randint(1, 4)
+            tickets = userPriorities[userName] * 10
+            cpu = randint(1,cpus[2]) # Has cpus from 1 to 16
+            gpu = randint(0,gpus[2])
+            ram = randint(2,rams[2])
 
             currentTime += arrivalTime
             f.write("%s %s %d %d %d %d %d %d %d" % (userName, podName, currentTime, work, prio, tickets, cpu, gpu, ram))
 
-            
 if __name__ == "__main__":
    main(sys.argv[1:])
